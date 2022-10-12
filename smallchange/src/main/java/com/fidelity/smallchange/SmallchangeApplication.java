a28c57b94fa5e85f9fdc1b42971bec549b2dfa3c
DhivakarK-git <dhivakar189@gmail.com>
2022-10-12 11:14:36 +0530

    first commit

---
+++ smallchange/src/main/java/com/fidelity/smallchange/SmallchangeApplication.java
@@ -0,0 +1,13 @@
+package com.fidelity.smallchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallchangeApplication.class, args);
+	}
+
+}
---
+++ smallchange/src/test/java/com/fidelity/smallchange/SmallchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fidelity.smallchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

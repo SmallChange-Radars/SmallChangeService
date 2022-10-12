cf0916c91622b7a17110a1c036d2b1c011745fe8
DhivakarK-git <dhivakar189@gmail.com>
2022-10-12 12:44:01 +0530

    gsutuba

--- smallchange/src/main/java/com/fidelity/smallchange/model/Price.java
+++ smallchange/src/main/java/com/fidelity/smallchange/model/Price.java
@@ -1,13 +1,44 @@
 package com.fidelity.smallchange.model;
 
+import java.math.BigDecimal;
 import java.util.Objects;
 
 public class Price {
 	private String instrumentId;
-	private double bidPrice;
-	private double askPrice;
+	private BigDecimal bidPrice;
+	private BigDecimal askPrice;
 	private String timestamp;
 	private Instrument instrument;
+	public String getInstrumentId() {
+		return instrumentId;
+	}
+	public void setInstrumentId(String instrumentId) {
+		this.instrumentId = instrumentId;
+	}
+	public BigDecimal getBidPrice() {
+		return bidPrice;
+	}
+	public void setBidPrice(BigDecimal bidPrice) {
+		this.bidPrice = bidPrice;
+	}
+	public BigDecimal getAskPrice() {
+		return askPrice;
+	}
+	public void setAskPrice(BigDecimal askPrice) {
+		this.askPrice = askPrice;
+	}
+	public String getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+	public Instrument getInstrument() {
+		return instrument;
+	}
+	public void setInstrument(Instrument instrument) {
+		this.instrument = instrument;
+	}
 	@Override
 	public int hashCode() {
 		return Objects.hash(askPrice, bidPrice, instrument, instrumentId, timestamp);
@@ -21,8 +52,7 @@ public class Price {
 		if (getClass() != obj.getClass())
 			return false;
 		Price other = (Price) obj;
-		return Double.doubleToLongBits(askPrice) == Double.doubleToLongBits(other.askPrice)
-				&& Double.doubleToLongBits(bidPrice) == Double.doubleToLongBits(other.bidPrice)
+		return Objects.equals(askPrice, other.askPrice) && Objects.equals(bidPrice, other.bidPrice)
 				&& Objects.equals(instrument, other.instrument) && Objects.equals(instrumentId, other.instrumentId)
 				&& Objects.equals(timestamp, other.timestamp);
 	}
@@ -30,13 +60,5 @@ public class Price {
 	public String toString() {
 		return "Price [instrumentId=" + instrumentId + ", bidPrice=" + bidPrice + ", askPrice=" + askPrice
 				+ ", timestamp=" + timestamp + ", instrument=" + instrument + "]";
-	}
-	public Price(String instrumentId, double bidPrice, double askPrice, String timestamp, Instrument instrument) {
-		super();
-		this.instrumentId = instrumentId;
-		this.bidPrice = bidPrice;
-		this.askPrice = askPrice;
-		this.timestamp = timestamp;
-		this.instrument = instrument;
 	}
 }

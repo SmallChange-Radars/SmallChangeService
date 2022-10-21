ae756af1e384d44df287fe4fec1828ca1c4c552b
amrutha-manne <amruthamanne19dec@gmail.com>
2022-10-21 10:19:26 +0530

    client db model

---
+++ smallchange/src/main/java/com/fidelity/smallchange/model/ClientDB.java
@@ -0,0 +1,10 @@
+package com.fidelity.smallchange.model;
+
+import java.math.BigDecimal;
+
+public class ClientDB {
+	Client client;
+	String password;
+	BigDecimal wallet;
+	String walletCurrency;
+}

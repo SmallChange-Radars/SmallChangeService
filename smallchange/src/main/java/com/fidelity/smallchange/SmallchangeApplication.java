d0a440f85f2f26f7f19d6240be2c16a97f6959b5
DhivakarK-git <dhivakar189@gmail.com>
2022-10-21 14:51:55 +0530

    FMTS stuff

--- smallchange/src/main/java/com/fidelity/smallchange/SmallchangeApplication.java
+++ smallchange/src/main/java/com/fidelity/smallchange/SmallchangeApplication.java
@@ -1,30 +1,21 @@
 package com.fidelity.smallchange;
-
-import java.util.List;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.fidelity.smallchange.integration.mapper.PortfolioMapper;
-import com.fidelity.smallchange.model.Portfolio;
+import com.fidelity.smallchange.integration.FMTSRestClient;
+import com.fidelity.smallchange.model.Client;
 
 @SpringBootApplication
-@MapperScan(basePackages = { "com.fidelity.smallchange.integration.mapper" }, annotationClass = Mapper.class)
 public class SmallchangeApplication implements CommandLineRunner {
 	
-//	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	
 	@Autowired
-    private PortfolioMapper port;
+    private FMTSRestClient a;
 	
 	@Override
     public void run(String...args) throws Exception {
-		List<Portfolio> p = port.getAllPortfolios();
-        System.out.println(p.toString());
+        System.out.println(a.clientVerification(new Client("", "dd@gmail.com", null, null, null, null, "")).toString());
     }
 
 	public static void main(String[] args) {

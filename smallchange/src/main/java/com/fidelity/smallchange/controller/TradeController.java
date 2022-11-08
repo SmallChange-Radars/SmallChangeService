55aa628cfce387871563de8b6a6e9749806f9793
DhivakarK-git <dhivakar189@gmail.com>
2022-11-08 21:23:41 +0530

    added orderdata.json

--- smallchange/src/main/java/com/fidelity/smallchange/controller/TradeController.java
+++ smallchange/src/main/java/com/fidelity/smallchange/controller/TradeController.java
@@ -30,43 +30,44 @@ import com.fidelity.smallchange.service.UserDetailsImpl;
 @RestController
 @RequestMapping("/api")
 public class TradeController {
-	
+
 	@Autowired
 	private TradeService tradeService;
-	
-	
-	@GetMapping(value="/tradeActivity", produces=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<Trade>> getTradeActivity(@AuthenticationPrincipal UserDetailsImpl userDetails,@RequestParam(required = false) String q,
-			@RequestParam(required = false) Integer _page, @RequestParam(required = false) Integer _limit,
-			 @RequestParam(required = false) String _sort,
-			@RequestParam(required = false) String _order){
+
+	@GetMapping(value = "/tradeActivity", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Trade>> getTradeActivity(@AuthenticationPrincipal UserDetailsImpl userDetails,
+			@RequestParam(required = false) String q, @RequestParam(required = false) Integer _page,
+			@RequestParam(required = false) Integer _limit, @RequestParam(required = false) String _sort,
+			@RequestParam(required = false) String _order) {
 		try {
-			
-			List<Trade> tradeHistory = tradeService.getTradeActivityByClientId(userDetails.getClientId(),q, _page, _limit, _sort, _order);
+
+			List<Trade> tradeHistory = tradeService.getTradeActivityByClientId(userDetails.getClientId(), q, _page,
+					_limit, _sort, _order);
 			int totalTradesCount = tradeService.totalTradesByClientId(userDetails.getClientId());
 			HttpHeaders responseHeaders = new HttpHeaders();
 			responseHeaders.set("Access-Control-Expose-Headers", "X-Total-Count");
 			responseHeaders.set("X-Total-Count", String.valueOf(totalTradesCount));
-			if(tradeHistory.size()==0 || tradeHistory == null) {
+			if (tradeHistory.size() == 0 || tradeHistory == null) {
 				return ResponseEntity.noContent().build();
 			}
 			return ResponseEntity.ok().headers(responseHeaders).body(tradeHistory);
-		}catch(Exception e) {
-			throw new ServerErrorException("Error while conencting to DB",e);
+		} catch (Exception e) {
+			throw new ServerErrorException("Error while conencting to DB", e);
 		}
 	}
-	
-	@PostMapping(value="/tradeExecution", produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<?> tradeExecution(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody Order order){
+
+	@PostMapping(value = "/tradeExecution", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> tradeExecution(@AuthenticationPrincipal UserDetailsImpl userDetails,
+			@RequestBody Order order) {
 		try {
-			if(tradeService.tradeExecution(order, userDetails.getClientId())) {
+			if (tradeService.tradeExecution(order, userDetails.getClientId())) {
 				return ResponseEntity.status(HttpStatus.OK).body(new MessageResponse("Trade executed successfully"));
 			}
-			return ResponseEntity.status(HttpStatus.CONFLICT).body(new MessageResponse("Price mismatch or unavailable resources to execute Trade."));			
-		}catch(Exception e) {
-			throw new ServerErrorException("Error while conencting to DB",e);
+			return ResponseEntity.status(HttpStatus.CONFLICT)
+					.body(new MessageResponse("Price mismatch or unavailable resources to execute Trade."));
+		} catch (Exception e) {
+			throw new ServerErrorException("Error while conencting to DB", e);
 		}
-		
-		
+
 	}
 }

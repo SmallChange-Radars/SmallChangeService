9fa9b58e6636ba24fa2488f7caaa06ad3b16bc97
shreeshyadav <shreeshyadav@gmail.com>
2022-11-10 13:54:57 +0530

    portfolio controller test added

--- smallchange/src/test/java/com/fidelity/smallchange/controller/PortfolioControllerTest.java
+++ smallchange/src/test/java/com/fidelity/smallchange/controller/PortfolioControllerTest.java
@@ -1,9 +1,14 @@
 package com.fidelity.smallchange.controller;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.math.BigDecimal;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,15 +20,18 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
 
+import com.fidelity.smallchange.SmallchangeApplication;
 import com.fidelity.smallchange.model.ClientDB;
+import com.fidelity.smallchange.model.ClientPortfolio;
 import com.fidelity.smallchange.model.Country;
+import com.fidelity.smallchange.model.Trade;
 
-@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
-@Sql({"classpath:schema.sql"}) 
+@SpringBootTest(classes = SmallchangeApplication.class, webEnvironment = WebEnvironment.RANDOM_PORT)
 class PortfolioControllerTest {
 	
 	@Autowired
@@ -32,21 +40,25 @@ class PortfolioControllerTest {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
-//	HttpEntity<ClientDB> request;
-//	ResponseEntity<String> response;
+	String token;
+	String clientId = "290988199";
 	
 	//endtoend only
-	@BeforeAll
+	@BeforeEach
 	void setUp() throws Exception {
-		ClientDB client = new ClientDB("", "dave101@gmail.com", "19260818", Country.of("US"), "90210", null, null,
-				"itsasecret", BigDecimal.TEN, "USD", "ROLE_CLIENT");
+		JSONParser jsonParser = new JSONParser();
 
+		ClientDB client = new ClientDB("", "a000007@gmail.com", "19260818", Country.of("US"), "90210", null, null,
+				"itsasecret101", BigDecimal.TEN, "USD", "ROLE_CLIENT");
 		HttpHeaders headers = new HttpHeaders();
 		headers.set("Content-Type", "application/json");
 
 		HttpEntity<ClientDB> request = new HttpEntity<>(client, headers);
-		ResponseEntity<String> response = restTemplate.exchange("/api/auth/signup", HttpMethod.POST, request,
+		ResponseEntity<String> response = restTemplate.exchange("/api/auth/signin", HttpMethod.POST, request,
 				String.class);
+		Object obj = jsonParser.parse(response.getBody());
+		JSONObject toke = (JSONObject) obj;
+		token = toke.get("accessToken").toString();
 	}
 
 	@AfterEach
@@ -54,8 +66,25 @@ class PortfolioControllerTest {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testGetPortfolio() {
+		String url = "/api/portfolio";
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Content-Type", "application/json");
+		headers.set("Authorization", "Bearer " + token);
+		HttpEntity<?> request = new HttpEntity<>(headers);
+		ResponseEntity<ClientPortfolio[]> responseEntity = restTemplate.exchange(url, HttpMethod.GET, request, ClientPortfolio[].class);
+		assertThat(responseEntity.getStatusCode(), is(equalTo(HttpStatus.OK)));
+	}
+	
+	@Test
+	void testGetPortfolio_Exception() {
+		String url = "/api/portfolio";
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Content-Type", "application/json");
+		//headers.set("Authorization", "Bearer " + token);
+		HttpEntity<?> request = new HttpEntity<>(headers);
+		ResponseEntity<ClientPortfolio[]> responseEntity = restTemplate.exchange(url, HttpMethod.GET, request, ClientPortfolio[].class);
+		assertThat(responseEntity.getStatusCode(), is(equalTo(HttpStatus.UNAUTHORIZED)));
 	}
 
 }
--- smallchange/src/test/java/com/fidelity/smallchange/service/PortfolioServiceImplTest.java
+++ smallchange/src/test/java/com/fidelity/smallchange/service/PortfolioServiceImplTest.java
@@ -12,13 +12,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fidelity.smallchange.model.ClientPortfolio;
 import com.fidelity.smallchange.model.Portfolio;
 
 @SpringBootTest
-class PortfolioServiceImplTest {
+class PortfolioSserviceImplTest {
 	
 	@Autowired
-	private PortfolioService dao;
+	private PortfolioService service;
 
 	@BeforeEach
 	void setUp() throws Exception {
@@ -30,34 +32,43 @@ class PortfolioServiceImplTest {
 
 	@Test 
 	void testGetAllPortfolios() { 
-		List<Portfolio> p = dao.getAllPortfolios(); 
+		List<Portfolio> p = service.getAllPortfolios(); 
 		assertEquals(175, p.size()); 
 	}
 	
 	@Test
 	void testGetPortfolioByClientId() {
-		List<Portfolio> p = dao.getPortfolioByClientId("744385865");
+		List<Portfolio> p = service.getPortfolioByClientId("744385865");
 		assertEquals(66, p.size());
 	}
 	
 	@Test
 	@Transactional
 	void testInsertPortfolio() {
-		List<Portfolio> p = dao.getPortfolioByClientId("744385865");
+		List<Portfolio> p = service.getPortfolioByClientId("744385865");
 		assertEquals(66, p.size());
 		Portfolio newPort = new Portfolio("744385865", "T67890", 70, new BigDecimal("670.89"));
-		dao.insertPortfolio(newPort);
-		int newSize = dao.getPortfolioByClientId("744385865").size();
+		service.insertPortfolio(newPort);
+		int newSize = service.getPortfolioByClientId("744385865").size();
 		assertEquals(67, newSize);
 	}
 	
 	@Test
 	void testCheckAvailableQuantity() {
-		assertTrue(dao.checkAvailableQuantity("744385865", "MNST", 30));
+		assertTrue(service.checkAvailableQuantity("744385865", "MNST", 30));
 	}
 	
 	@Test
 	void testCheckAvailableQuantityReturnsFalse() {
-		assertFalse(dao.checkAvailableQuantity("744385865", "MNST", 300000));
+		assertFalse(service.checkAvailableQuantity("744385865", "MNST", 300000));
 	}
+	
+	@Test
+    void testGetPortfolio() throws JsonProcessingException {
+        Pair<List<ClientPortfolio>, List<BigDecimal>> pair =
+                service.getClientPortfolio("744385865");
+        assertEquals(66, pair.getFirst().size());
+        assertEquals(new BigDecimal("1986855.91"), pair.getSecond().get(0));
+        assertEquals(new BigDecimal("-19671.86"), pair.getSecond().get(1));
+    }
 }
